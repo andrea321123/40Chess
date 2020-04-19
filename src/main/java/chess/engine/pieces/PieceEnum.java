a4299ca5f245456ecba7ed8c941b46073f8db9b1
Andrea Galvan <gal.andrea.van@gmail.com>
2020-04-19 15:48:55 +0200

    Updated PieceEnum.java

--- src/main/java/chess/engine/pieces/PieceEnum.java
+++ src/main/java/chess/engine/pieces/PieceEnum.java
@@ -2,7 +2,14 @@ package chess.engine.pieces;
 
 /**
  * Enum of all possible pieces
+ * @author Andrea Galvan
+ * @version 1.1
  */
 public enum PieceEnum {
-
+    KING,
+    QUEEN,
+    ROOK,
+    KNIGHT,
+    BISHOP,
+    PAWN
 }
