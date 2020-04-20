518b709ee1b2c61d5cf50e94a299a6dcd4156d3c
Andrea Galvan <gal.andrea.van@gmail.com>
2020-04-20 12:03:26 +0200

    Added Queen.java

---
+++ src/main/java/chess/engine/pieces/Queen.java
@@ -0,0 +1,14 @@
+package chess.engine.pieces;
+
+import java.util.LinkedList;
+import myutil.MyPair;
+
+/**
+ * Implements a queen piece
+ * @author Andrea Galvan
+ * @version 1.0
+ */
+public class Queen extends Piece {
+    @Override
+    public LinkedList<MyPair<Integer, Integer>> possibleMoves() {return null;}
+}
