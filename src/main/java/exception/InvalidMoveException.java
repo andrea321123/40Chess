9f9007b11437c90e80fa08e1298391c8591f1239
Andrea Galvan <gal.andrea.van@gmail.com>
2020-04-20 11:38:53 +0200

    Added InvalidMoveException.java

---
+++ src/main/java/exception/InvalidMoveException.java
@@ -0,0 +1,10 @@
+package exception;
+
+/**
+ * Unchecked exception thrown when someone tries to move in an illegal position
+ * @author Andrea Galvan
+ * @version 1.0
+ */
+public class InvalidMoveException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
