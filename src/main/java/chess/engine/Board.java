59d81419a8981df3cbbf0587535261ffce76a6c5
Andrea Galvan <gal.andrea.van@gmail.com>
2020-04-20 11:31:14 +0200

    Updated Board.java
    Added getGrid(), removePiece() methods

--- src/main/java/chess/engine/Board.java
+++ src/main/java/chess/engine/Board.java
@@ -7,7 +7,7 @@ import chess.engine.pieces.Piece;
  * Implementation of a chess board
  * 
  * @author Andrea Galvan
- * @version 1.2
+ * @version 1.3
  */
 public class Board {
     // fields
@@ -35,11 +35,27 @@ public class Board {
                     grid[i][j] = board.grid[i][j].getClass().getConstructor(Piece.class, Board.class).newInstance(board.grid[i][j], this);
     }
 
+    //getter/setter
+    public Piece[][] getGrid(){
+        return grid;
+    }
+
     /**
      * Add piece parameter to the board
      * @param piece piece to add
      */
     public void addPiece(Piece piece){
         grid[piece.getPosition().getFirst()][piece.getPosition().getSecond()] = piece;
+        piece.setBoard(this);
+    }
+    /**
+     * Remove piece at position parameter
+     * @param position position of the piece to remove
+     */
+    public void removePiece(Piece piece){
+        if(piece.getBoard() == this){
+            grid[piece.getPosition().getFirst()][piece.getPosition().getSecond()] = null;
+            piece.setBoard(null);
+        }
     }
 }
