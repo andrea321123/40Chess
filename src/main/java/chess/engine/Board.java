1f8b70a8c95946c45c3492d2abb4ef9d8c98c68c
Andrea Galvan <gal.andrea.van@gmail.com>
2020-04-21 23:24:14 +0200

    Updated Board.java
    Added initialize(), piecesByColour(), toString() methods

--- src/main/java/chess/engine/Board.java
+++ src/main/java/chess/engine/Board.java
@@ -1,13 +1,15 @@
 package chess.engine;
 
 import java.lang.reflect.InvocationTargetException;
-import chess.engine.pieces.Piece;
+import java.util.LinkedList;
+import chess.engine.pieces.*;
+import myutil.MyPair;
 
 /**
  * Implementation of a chess board
  * 
  * @author Andrea Galvan
- * @version 1.4
+ * @version 1.5
  */
 public class Board {
     // fields
@@ -20,7 +22,6 @@ public class Board {
     public Board() {
         grid = new Piece[SIZE][SIZE];
     }
-
     /**
      * Copy constructor
      * 
@@ -41,6 +42,35 @@ public class Board {
     }
 
     /**
+     * Initializes the grid with standard starting configuration pieces
+     */
+    public void initialize(){
+        for(int j = 0; j < SIZE; j++){      //pawns
+            addPiece(new Pawn(ColourEnum.WHITE, this, new MyPair<>(SIZE -2, j)));
+            addPiece(new Pawn(ColourEnum.BLACK, this, new MyPair<>(1, j)));
+        }
+        
+        //other pieces
+        addPiece(new Rook(ColourEnum.WHITE, this, new MyPair<>(7, 0)));
+        addPiece(new Rook(ColourEnum.WHITE, this, new MyPair<>(7, 7)));
+        addPiece(new Knight(ColourEnum.WHITE, this, new MyPair<>(7, 1)));
+        addPiece(new Knight(ColourEnum.WHITE, this, new MyPair<>(7, 6)));
+        addPiece(new Bishop(ColourEnum.WHITE, this, new MyPair<>(7, 2)));
+        addPiece(new Bishop(ColourEnum.WHITE, this, new MyPair<>(7, 5)));
+        addPiece(new King(ColourEnum.WHITE, this, new MyPair<>(7, 4)));
+        addPiece(new Queen(ColourEnum.WHITE, this, new MyPair<>(7, 3)));
+
+        addPiece(new Rook(ColourEnum.BLACK, this, new MyPair<>(0, 0)));
+        addPiece(new Rook(ColourEnum.BLACK, this, new MyPair<>(0, 7)));
+        addPiece(new Knight(ColourEnum.BLACK, this, new MyPair<>(0, 1)));
+        addPiece(new Knight(ColourEnum.BLACK, this, new MyPair<>(0, 6)));
+        addPiece(new Bishop(ColourEnum.BLACK, this, new MyPair<>(0, 2)));
+        addPiece(new Bishop(ColourEnum.BLACK, this, new MyPair<>(0, 5)));
+        addPiece(new King(ColourEnum.BLACK, this, new MyPair<>(0, 4)));
+        addPiece(new Queen(ColourEnum.BLACK, this, new MyPair<>(0, 3)));
+    }
+
+    /**
      * Add piece parameter to the board
      * @param piece piece to add
      */
@@ -57,5 +87,65 @@ public class Board {
             grid[piece.getPosition().getFirst()][piece.getPosition().getSecond()] = null;
             piece.setBoard(null);
         }
+    }
+
+    /**
+     * Returns a list containing all pieces of colour parameter
+     * @param colour colour of the pieces to be searched
+     * @return black pieces
+     */
+    public LinkedList<Piece> piecesByColour(ColourEnum colour){
+        LinkedList<Piece> pieces = new LinkedList<>();
+
+        for(int i = 0; i < SIZE; i++)
+            for(int j = 0; j < SIZE; j++)
+                try{
+                    if(grid[i][j].getColour() == colour)
+                        pieces.add(grid[i][j]);
+                } catch (NullPointerException e) {}
+
+        return pieces;
+    }
+
+    @Override
+    public String toString() {
+        String string = "";
+
+        for (int i = 0; i < SIZE; i++) {
+            string += "|";
+            for (int j = 0; j < SIZE; j++) {
+                try {
+                    Piece nowPiece = grid[i][j];
+                    switch (nowPiece.getPiece()) {
+                        case KING:
+                            string += nowPiece.getColour() == ColourEnum.WHITE ? "K " : "k ";
+                            break;
+                        case QUEEN:
+                            string += nowPiece.getColour() == ColourEnum.WHITE ? "Q " : "q ";
+                            break;
+                        case BISHOP:
+                            string += nowPiece.getColour() == ColourEnum.WHITE ? "B " : "b ";
+                            break;
+                        case ROOK:
+                            string += nowPiece.getColour() == ColourEnum.WHITE ? "R " : "r ";
+                            break;
+                        case KNIGHT:
+                            string += nowPiece.getColour() == ColourEnum.WHITE ? "H " : "h ";
+                            break;
+                        case PAWN:
+                            string += nowPiece.getColour() == ColourEnum.WHITE ? "P " : "p ";
+                            break;
+                        default:
+                            string += "  ";
+                            break;
+                    }
+                }
+                catch(NullPointerException e){
+                    string += "  ";
+                }
+            }
+            string += "|\n";
+        }
+        return string;
     }
 }
--- src/test/java/chess/engine/BoardTest.java
+++ src/test/java/chess/engine/BoardTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 /**
  * @author Andrea Galvan
- * @version 1.1
+ * @version 1.2
  */
 public class BoardTest {
     @Test
@@ -19,7 +19,7 @@ public class BoardTest {
         test.addPiece(new Queen(ColourEnum.BLACK, test, new MyPair<Integer,Integer>(5, 7)));
         test.addPiece(new Rook(ColourEnum.BLACK, test, new MyPair<Integer,Integer>(6, 2)));
 
-        assertTrue(test.getGrid()[2][1].getColor() == ColourEnum.WHITE);
+        assertTrue(test.getGrid()[2][1].getColour() == ColourEnum.WHITE);
         assertTrue(test.getGrid()[2][2].getPiece() == PieceEnum.QUEEN);
         assertTrue(test.getGrid()[5][7].getPosition().equals(new MyPair<>(5, 7)));
         assertTrue(test.getGrid()[6][2].getPiece() != PieceEnum.QUEEN);
@@ -36,5 +36,16 @@ public class BoardTest {
 
         assertTrue(test.getGrid()[2][1] != null);
         assertTrue(test.getGrid()[2][2] == null);
+    }
+    @Test
+    public void PiecesByColourTest(){
+        Board test = new Board();
+        test.initialize();
+        
+        assertTrue(test.piecesByColour(ColourEnum.WHITE).size() == 16);
+        assertTrue(test.piecesByColour(ColourEnum.BLACK).size() == 16);
+
+        test.addPiece(new Queen(ColourEnum.WHITE, test, new MyPair<>(4, 4)));
+        assertTrue(test.piecesByColour(ColourEnum.WHITE).size() == 17);
     }
 }
