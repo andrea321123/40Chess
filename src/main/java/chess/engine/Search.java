9be9261437c1905b25139209fdc99a8c718ca76e
Andrea Galvan <gal.andrea.van@gmail.com>
2020-04-25 10:04:42 +0200

    Added Search.java
    Added Search.java class and its test

---
+++ src/main/java/chess/engine/Search.java
@@ -0,0 +1,14 @@
+package chess.engine;
+
+import java.util.LinkedList;
+import myutil.*;
+
+/**
+ * Search the best move from a given position
+ * @author Andrea Galvan
+ * @version 1.0
+ */
+public class Search {
+    private MyTree<Board> tree;
+    public static final int DEPTH = 2;
+}
